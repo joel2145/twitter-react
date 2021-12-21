f9b370ce17e1b2c4ffd6f289dda75489a90c86e3
joel2145 <kikkun0513@gmail.com>
2021-12-21 16:55:23 +0900

    新規コメント機能を実装

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import firebase from "firebase/compat/app";
 import { useSelector } from "react-redux";
-import { makeStyles } from "@material-ui/core";
+import { Button, IconButton, makeStyles } from "@material-ui/core";
 import { Send } from "@material-ui/icons";
 
 import styles from "./Post.module.css";
@@ -18,6 +18,20 @@ interface Props {
 
 export const Post: React.FC<Props> = (props) => {
   const { postId, image, text, timestamp, username } = props;
+  const user = useSelector(selectUser);
+  const [comment, setComment] = useState("");
+
+  const newComment = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    db.collection("posts").doc(postId).collection("comments").add({
+      text: comment,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      username: user.displayName,
+    });
+    setComment("");
+  };
+
   return (
     <>
       <div className={styles.post}>
@@ -25,7 +39,6 @@ export const Post: React.FC<Props> = (props) => {
           <div className={styles.post_header}>
             <h3>
               <span className={styles.post_headerUser}>@{username}</span>
-              {/* {console.log(new Date(timestamp?.toDate()).toLocalString())} */}
               <span className={styles.post_headerTime}>
                 {new Date(timestamp?.toDate()).toLocaleString("ja")}
               </span>
@@ -39,6 +52,29 @@ export const Post: React.FC<Props> = (props) => {
               <img src={image} alt="tweet" />
             </div>
           )}
+
+          {/* コメント機能 */}
+          <form onSubmit={newComment}>
+            <div className={styles.post_form}>
+              <input
+                className={styles.post_input}
+                placeholder="コメントする"
+                type="text"
+                // autoFocus
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+              ></input>
+              <button
+                type="submit"
+                disabled={!comment}
+                className={
+                  comment ? styles.post_button : styles.post_buttonDisable
+                }
+              >
+                <Send></Send>
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
